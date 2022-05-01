bea676a8933300575d35b50b117929601966cdda
Areeba <areebamuzammil@gmail.com>
2022-05-01 09:33:38 +0500

    counter code added

--- counterApp/app/src/main/java/com/example/counterapp/MainActivity.java
+++ counterApp/app/src/main/java/com/example/counterapp/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.counterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonIncrease,ButtonDecrease,ButtonReset;
+    TextView textview;
+    int counter=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonIncrease=findViewById(R.id. button5);
+        ButtonDecrease=findViewById(R.id. button8);
+        ButtonReset=findViewById(R.id. button7);
+        textview=findViewById(R.id. textView);
+        buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                textview.setText(Integer.toString(counter));
+            }
+        });
+        ButtonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter--;
+                textview.setText(Integer.toString(counter));
+            }
+        });
+        ButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter=0;
+                textview.setText(Integer.toString(counter));
+            }
+        });
     }
 }
