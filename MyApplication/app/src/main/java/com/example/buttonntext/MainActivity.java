801a6f70b1c05c58ffe1ad9bc72cda4a0c3b7bc5
Areeba <areebamuzammil@gmail.com>
2022-05-01 08:32:52 +0500

    click func added

--- MyApplication/app/src/main/java/com/example/buttonntext/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/buttonntext/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.buttonntext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn=findViewById(R.id.button);
+        TextView txt=findViewById(R.id.textView);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setText("I'm Fine");
+            }
+        });
     }
 }
