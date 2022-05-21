1d6b6fe3f5c2972a6ec585d1382255a4a5da3a86
Areeba <areebamuzammil@gmail.com>
2022-05-21 15:30:47 +0500

    repo link added

--- Assignment1/app/src/main/java/com/example/assignment1/MainActivity.java
+++ Assignment1/app/src/main/java/com/example/assignment1/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonLearn,buttonExam,buttonRepo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonLearn=findViewById(R.id. button);
+        buttonExam=findViewById(R.id. button2);
+        buttonRepo=findViewById(R.id. button3);
+
+        buttonRepo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/areebamuzammil/M041_MC"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+               // counter++;
+               // textview.setText(Integer.toString(counter));
+            }
+        });
     }
 }
